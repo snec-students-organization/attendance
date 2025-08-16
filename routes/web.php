3ca1436a3f9e58402a763c4520e805b1fd6b2235
rameesmk <monuramees55@gmail.com>
2025-08-16 23:12:09 +0530

    Revert "reset again"
    This reverts commit 583aa83ab59ad28fc8fc5137b4866acfedb6c09c.

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceController;
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/attendance/create', [AttendanceController::class, 'create'])->name('attendance.create');
+Route::post('/attendance/store', [AttendanceController::class, 'store'])->name('attendance.store');
+Route::post('/attendance/store-all-periods', [AttendanceController::class, 'storeAllPeriods'])->name('attendance.storeAllPeriods');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/attendance/students/{classId}', [AttendanceController::class, 'getStudents'])->name('attendance.students');
+Route::get('/attendance/view', [AttendanceController::class, 'index'])->name('attendance.view');
+Route::get('/attendance/summary', [AttendanceController::class, 'summaryByDate'])->name('attendance.summary');
+Route::get('/attendance/summary/export', [AttendanceController::class, 'exportSummaryPDF'])->name('attendance.summary.export');
+Route::get('/attendance/teacher-student-summary', [AttendanceController::class, 'teacherStudentSummary'])
+    ->name('attendance.teacherStudentSummary');
